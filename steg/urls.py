b0e77c0c37d46e864f536c1f908b10daaf339fa7
PoornaHegde20 <76869715+PoornaHegde20@users.noreply.github.com>
2022-03-27 21:25:31 +0530

    urls updated

--- steg/urls.py
+++ steg/urls.py
@@ -6,12 +6,15 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'steg'
 urlpatterns = [
+    # path('upload', views.index_encode, name='upload'),
     path('', views.index, name='index'),
-    # path('', views.showImage, name='upload')
-    path('/decode', views.decode, name='decode'),
-    path('/encode', views.encode, name='encode'),
-    path('/lsbDecode', views.lsbDecode, name='lsbDecode'),
-    path('/lsbEncode', views.lsbEncode, name='lsbEncode'),
+
+    path('show-image/<int:id>/', views.show_image, name='show_img'),
+    path('decode', views.index_decode, name='decode'),
+    path('encode', views.index_encode, name='encode'),
+    path('lsbDecode', views.lsbDecode, name='lsbDecode'),
+    path('lsbEncode', views.lsbEncode, name='lsbEncode'),
 
 ]
