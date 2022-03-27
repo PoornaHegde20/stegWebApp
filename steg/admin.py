1aee234ad85fffaa953723e93618946fc2ae4c14
PoornaHegde20 <76869715+PoornaHegde20@users.noreply.github.com>
2022-03-27 21:24:36 +0530

    django minor

--- steg/admin.py
+++ steg/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from steg.models import LSBEncode
 # Register your models here.
+admin.site.register(LSBEncode)
+from steg.models import LSBDecode
+admin.site.register(LSBDecode)
