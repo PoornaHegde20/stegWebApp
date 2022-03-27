801bec7e1b65ee9ebaa8a1fb6fbf26b4011dfaa4
PoornaHegde20 <76869715+PoornaHegde20@users.noreply.github.com>
2022-03-27 21:25:17 +0530

    updated models

--- steg/models.py
+++ steg/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class lsbEncoding(models.Model):
-    id=models.AutoField(primary_key=True)
-    inputImageName=models.CharField(max_length=100)
-    inputImagePath=models.FileField(upload_to='media/images/', null=True, verbose_name="")
-    text=models.TextField(max_length=250)
-    outputImageName=models.CharField(max_length=100)
-    outputImagePath=models.FileField(upload_to='media/images/', null=True, verbose_name="")
+
+
+class LSBEncode(models.Model):
+    id = models.AutoField(primary_key=True)
+    inputImagePath = models.FileField(
+        upload_to='images/', null=False, verbose_name="")
+    outputImagePath = models.FileField(
+        upload_to='images/', null=True, verbose_name="")
+
     def __str__(self):
-        return self.inputImageName
+        return str(self.inputImagePath) + " ID : " + str(self.id)
+    
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('steg:show_img', kwargs={'id': self.id})
 
 
-class lsbDecoding(models.Model):
-    id=models.AutoField(primary_key=True)
-    inputImageName=models.CharField(max_length=100)
-    inputImagePath=models.FileField(upload_to='media/images/', null=True, verbose_name="")
-    text=models.TextField(max_length=250)
+class LSBDecode(models.Model):
+    id = models.AutoField(primary_key=True)
+    inputImagePath = models.FileField(
+        upload_to='images/', null=True, verbose_name="")
+    # text = models.TextField(max_length=250)
+
     def __str__(self):
-        return self.inputImageName
-
-    
-
-    
+        return str(self.inputImagePath)
