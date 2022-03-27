d7281f66298a71a6a5dcc3860de52ee408a5f4f4
PoornaHegde20 <76869715+PoornaHegde20@users.noreply.github.com>
2022-03-27 21:24:57 +0530

    updated urls

--- setgano/urls.py
+++ setgano/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('steg/', include('steg.urls', namespace='steg')),
+    path('steg/', include('steg.urls',namespace='steg')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
