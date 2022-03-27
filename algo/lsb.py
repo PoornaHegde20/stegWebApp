6ed4fb6de05927e2d29fd770629c1214025ccd44
PoornaHegde20 <76869715+PoornaHegde20@users.noreply.github.com>
2022-03-27 21:24:09 +0530

    updated algos

---
+++ algo/lsb.py
@@ -0,0 +1,76 @@
+import os
+import sys
+
+import numpy as np
+from PIL import Image
+
+np.set_printoptions(threshold=sys.maxsize)
+
+
+def lsb_decode_algo(source_path):
+    img = Image.open(source_path, 'r')
+    array = np.array(list(img.getdata()))
+
+    if img.mode == 'RGB':
+        n = 3
+    elif img.mode == 'RGBA':
+        n = 4
+
+    total_pixels = array.size//n
+
+    hidden_bits = ""
+    for p in range(total_pixels):
+        for q in range(0, 3):
+            hidden_bits += (bin(array[p][q])[2:][-1])
+
+    hidden_bits = [hidden_bits[i:i+8] for i in range(0, len(hidden_bits), 8)]
+
+    message = ""
+    for i in range(len(hidden_bits)):
+        if message[-5:] == "$t3g0":
+            break
+        else:
+            message += chr(int(hidden_bits[i], 2))
+    if "$t3g0" in message:
+        return message[:-5]
+    else:
+        return ""
+
+
+def lsb_encode_algo(source_path, message):
+    img = Image.open(source_path, 'r')
+    # destination_path = os.path.splitext(source_path)[0]+"Encrypted.png"
+    destination_path = source_path+"Encrypted.png"
+    width, height = img.size
+    array = np.array(list(img.getdata()))
+
+    if img.mode == 'RGB':
+        n = 3
+    elif img.mode == 'RGBA':
+        n = 4
+
+    total_pixels = array.size//n
+
+    message += "$t3g0"
+    b_message = ''.join([format(ord(i), "08b") for i in message])
+    req_pixels = len(b_message)
+
+    if req_pixels > total_pixels:
+        print("ERROR: Need larger file size")
+        return source_path
+
+    else:
+        index = 0
+        for p in range(total_pixels):
+            for q in range(0, 3):
+                if index < req_pixels:
+                    array[p][q] = int(
+                        bin(array[p][q])[2:9] + b_message[index], 2)
+                    index += 1
+
+        array = array.reshape(height, width, n)
+        enc_img = Image.fromarray(array.astype('uint8'), img.mode)
+        enc_img.save(destination_path)
+        # print("Image Encoded Successfully")
+
+    return destination_path
